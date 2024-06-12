86c8db3981c82b8fa48c75a204592b3d8e58c1fc
tbxark <tbxark@outlook.com>
2024-06-12 17:08:51 +0800

    chore: Update file extensions and remove console.log statements

--- api/status.js
+++ api/status.js
@@ -1,14 +1,9 @@
-export async function GET(request: Request) {
+export async function GET(request) {
     const url = new URL(request.url);
     const bvid = url.searchParams.get('bvid');
-    console.log("bvid", bvid)
     const cid = await fetch(`https://api.bilibili.com/x/player/pagelist?bvid=${bvid}&jsonp=jsonp`).then(r => r.json()).then(j => j.data[0].cid)
-    console.log("cid", cid)
     const online = await fetch(`https:api.bilibili.com/x/player/online/total?cid=${cid}&bvid=${bvid}`).then(r => r.json()).then(j => parseInt(j.data.total))
-    console.log("online", online)
     const status = await fetch(`https://api.bilibili.com/x/web-interface/view?bvid=${bvid}`).then(r => r.json())
-    console.log("status", status)
-
     const num2str = (num) => {
         if (num >= 10000) {
             return `${(num / 10000).toFixed(1)}万`
